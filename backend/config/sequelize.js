8cfeec68a71243227fff62711c36db711c42591c
alex_Palladis <alehandro4443@gmail.com>
2024-12-19 19:46:36 +0200

    heroku db

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false,
-  }
-);
+// Use the environment variable provided by Heroku
+const sequelize = new Sequelize(process.env.JAWSDB_URL, {
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // This depends on your database's SSL configuration
+    },
+  },
+});
 
 
 
